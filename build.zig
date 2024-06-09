71a42aa6fd89b02173bd66e1aa2484e0d4d2efe0
Peter Hellberg <peter@c7.se>
2024-06-09 18:51:06 +0200

    build: Use b.path

--- build.zig
+++ build.zig
@@ -2,8 +2,6 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     _ = b.addModule("w4", .{
-        .root_source_file = .{
-            .path = "src/wasm4.zig",
-        },
+        .root_source_file = b.path("src/wasm4.zig"),
     });
 }
